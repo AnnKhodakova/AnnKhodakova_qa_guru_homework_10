71923a2f47e07b569b7a0f6cfa67a9f3ac588e5f
Ann Khodakova <annkhodakova88@gmail.com>
2023-04-02 13:49:54 +0300

    fix

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,6 +1,7 @@
 from model.pages.registration_page import RegistrationPage
 from data.users import User
 import allure
+
 user = User('Bob', 'By',
             'email@email.email',
             'Male',
@@ -15,19 +16,21 @@ user = User('Bob', 'By',
 
 @allure.title("Registration page")
 def test_registration_page():
-        registration_page = RegistrationPage()
-        with allure.step("Open registrations form"):
-            registration_page.open()
-        with allure.step("Fill form"):
-            registration_page.user_registration(user)
-        with allure.step("Form results"):
-            registration_page.should_registered_user_with('Bob By',
-                                                  'email@email.email',
-                                                  'Male',
-                                                  '1234567890',
-                                                  '27 March,2002',
-                                                  'Physics',
-                                                  'Reading',
-                                                  'VYqbjvKLSlU.jpg',
-                                                  'address',
-                                                  'Haryana Karnal')
+    registration_page = RegistrationPage()
+    with allure.step("Open registrations form"):
+        registration_page.open()
+    with allure.step("Fill form"):
+        registration_page.user_registration(user)
+    with allure.step("Form results"):
+        registration_page.should_registered_user_with('Bob By',
+                                                      'email@email.email',
+                                                      'Male',
+                                                      '1234567890',
+                                                      '27 March,2002',
+                                                      'Physics',
+                                                      'Reading',
+                                                      'VYqbjvKLSlU.jpg',
+                                                      'address',
+                                                      'Haryana Karnal')
+
+
